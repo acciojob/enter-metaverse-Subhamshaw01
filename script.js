f9db3e4f8fd1f198bbe78f372929b013edf82015
Subhamshaw01 <137093444+Subhamshaw01@users.noreply.github.com>
2023-09-25 20:11:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+const b=document.getElementById("enterBtn")
+const para= document.getElementById("status")
+
+b.addEventListener("click", ()=> {
+	para.innerHTML="<h1>Enter the Metaverse</h1>";
+})
